b8175c6c5dbb93deb49537463d82dda2c3ddae9d
Gena Lazovskiy <genalazovskiy@MacBook-Pro-Gena.local>
2022-06-29 20:02:09 +0300

    Add Compassion

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './components/Home/Header/Header';
 import FirstScreen from './components/Home/FirstScreen/FirstScreen';
 import Footer from './components/Home/Footer/Footer';
 import Relevant from './components/Home/Relevant/Relevant';
+import Compassion from './components/Home/Compassion/Compassion';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header/>
       <FirstScreen/>
       <Relevant/>
+      <Compassion/>
       <Footer/>
     </>
     );
---
+++ src/components/Home/Compassion/Compassion.js
@@ -0,0 +1,40 @@
+import React from "react";
+import styles from '../Compassion/Compassion.module.scss';
+import Man from '../../../resources/img/compassion/icon1.png';
+import WomanMan from '../../../resources/img/compassion/icon2.png';
+import Woman from '../../../resources/img/compassion/icon3.png';
+
+const Compassion = () => {
+    return (
+        <div className={styles.compassion}>
+            <div className={styles.compassion__wrapper}>
+                <div className={styles.compassion__title}>love and compassion</div>
+                <div className={styles.compassion__descr}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur.
+                </div>
+                <div className={styles.compassion__box}>
+                    <div className={styles.compassion__box_imgMan}>
+                        <img src={Man} alt="Man" />
+                    </div>
+                    <div className={styles.compassion__box_btnImg}>
+                        <button>Read more</button>
+                        <img src={WomanMan} alt="WomanMan" />
+                    </div>
+                    <div className={styles.compassion__box_imgWoman}>
+                        <img src={Woman} alt="Woman" />
+                    </div>
+                </div>
+                <div className={styles.compassion__titleFooter}>celebrate WITH US</div>
+                <div className={styles.compassion__descrFooter}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+                <div className={styles.compassion__linkFooter}>Read More</div>
+            </div>
+        </div>
+    );
+};
+
+export default Compassion;
