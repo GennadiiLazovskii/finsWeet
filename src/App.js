21a36bac72e02d21c894ef301014ae0aefc74c35
Gena Lazovskiy <genalazovskiy@MacBook-Pro-Gena.local>
2022-07-09 13:28:50 +0300

    Add AboutUsFirstScrin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import Header from './components/Home/Header/Header';
-import FirstScreen from './components/Home/FirstScreen/FirstScreen';
+// import FirstScreen from './components/Home/FirstScreen/FirstScreen';
+// import Relevant from './components/Home/Relevant/Relevant';
+// import Compassion from './components/Home/Compassion/Compassion';
+// import BenefitsJoining from './components/Home/BenefitsJoining/BenefitsJoining';
+// import UpcomingEvent from './components/Home/UpcomingEvent/UpcomingEvent';
+// import AroundWorld from './components/Home/AroundWorld/AroundWorld';
+// import WatchAndListen from './components/Home/WatchAndListen/WatchAndListen';
+import AboutUsFirstScrin from './components/AboutUs/AboutUsFirstScrin/AboutUsFirstScrin';
 import Footer from './components/Home/Footer/Footer';
-import Relevant from './components/Home/Relevant/Relevant';
-import Compassion from './components/Home/Compassion/Compassion';
-import BenefitsJoining from './components/Home/BenefitsJoining/BenefitsJoining';
-import UpcomingEvent from './components/Home/UpcomingEvent/UpcomingEvent';
-import AroundWorld from './components/Home/AroundWorld/AroundWorld';
-import WatchAndListen from './components/Home/WatchAndListen/WatchAndListen';
 
 function App() {
   return (
     <>
       <Header/>
-      <FirstScreen/>
+      {/* <FirstScreen/>
       <Relevant/>
       <Compassion/>
       <BenefitsJoining/>
       <UpcomingEvent/>
       <AroundWorld/>
-      <WatchAndListen/>
+      <WatchAndListen/> */}
+
+      {/* AboutUS */}
+
+      <AboutUsFirstScrin/>
       <Footer/>
     </>
     );
---
+++ src/components/AboutUs/AboutUsFirstScrin/AboutUsFirstScrin.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './AboutUsFirstScrin.module.scss';
+
+const AboutUsFirstScrin = () => {
+
+    return ( 
+        <div className={styles.aboutUsFirstScrin}>
+            <div className={styles.aboutUsFirstScrin__wrapper}>
+                <div className={styles.aboutUsFirstScrin__title}>Serving the world around us</div>
+            </div>
+        </div>
+    );
+}
+
+export default AboutUsFirstScrin;
